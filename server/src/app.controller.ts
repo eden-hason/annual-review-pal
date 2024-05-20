a203eb425f4610edfe9f746f6fe1094caa5e0f38
eden-hason <eden@hason.me>
2024-05-20 22:01:53 +0300

    add new route

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import axios from 'axios';
 
@@ -17,6 +17,19 @@ export class AppController {
       'https://jira.tipalti.com:7000/rest/api/latest/search?jql=assignee=currentuser()',
       { headers: { Authorization: `Bearer ${process.env.JIRA_KEY}` } },
     );
+
+    return response.data;
+  }
+
+  @Get('/tickets-summary')
+  async getTicketsSummary(@Query('accessToken') accessToken) {
+    console.log('accessToken', accessToken);
+    const response = await axios.get(
+      'https://jira.tipalti.com:7000/rest/api/latest/search?jql=assignee=currentuser()',
+      { headers: { Authorization: `Bearer ${accessToken}` } },
+    );
+
+    return { ticketsAmount: 68, bugFixes: 22 };
 
     return response.data;
   }
